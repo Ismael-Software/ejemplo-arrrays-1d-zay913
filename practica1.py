cf431cbd30f3eb9b8ea2d5e6ad24631bda731f39
zay913 <va546667@uaeh.edu.mx>
2025-09-23 15:53:12 -0600

    este es la primera version

---
+++ practica1.py
@@ -0,0 +1,20 @@
+v=[3, 5, 1, 4, 8]
+pos=0
+ac=0
+while pos<len(v):
+    ac=ac+v[pos]
+    pos=pos+1
+
+print("la suma es: ", ac)
+
+
+v = []
+for i in range (5):
+    v2=int(input("Ingresa un numero: "))
+    v.append(v2)
+pos=0
+ac=0
+while pos<len(v):
+    ac=ac+v[pos]
+    pos=pos+1
+print("La suma es: ", ac)
