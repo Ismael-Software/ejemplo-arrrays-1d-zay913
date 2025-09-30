45da1dbf44685d2f6c46d92e966f691c89e37415
zay913 <va546667@uaeh.edu.mx>
2025-09-29 22:06:33 -0600

    Actualización del programa

--- practica1.py
+++ practica1.py
@@ -1,20 +1,19 @@
-v=[3, 5, 1, 4, 8]
-pos=0
-ac=0
-while pos<len(v):
-    ac=ac+v[pos]
-    pos=pos+1
-
-print("la suma es: ", ac)
-
-
-v = []
-for i in range (5):
-    v2=int(input("Ingresa un numero: "))
+v = []  
+n = int(input("¿Cuántos números deseas ingresar?: "))
+for i in range(n):
+    v2 = int(input(f"Ingresa el número {i+1}: "))
     v.append(v2)
-pos=0
-ac=0
-while pos<len(v):
-    ac=ac+v[pos]
-    pos=pos+1
-print("La suma es: ", ac)
+pos = 0
+ac = 0
+while pos < len(v):
+    ac = ac + v[pos]
+    pos = pos + 1
+print("La suma es:", ac)
+mayor = v[0]  
+pos = 1
+while pos < len(v):
+    if v[pos] > mayor:
+        mayor = v[pos]
+    pos = pos + 1
+
+print("El número mayor es:", mayor)
